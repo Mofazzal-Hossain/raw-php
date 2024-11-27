25e281295ea3b4cd55125a4ec11708527c8f8944
Mofazzal Hossain <mosharofmd74@gmail.com>
2024-11-27 10:13:37 +0600

    count, sizeof, and array count values

---
+++ count-sizeof-function.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * 
+ * Count & Sizeof Function in PHP
+ * 
+ */
+
+$employees = [
+    [1, 'Mofazzal', 'Developer', 20, 15000],
+    [2, 'Khan', 'UI/UX', 24, 25000],
+    [3, 'Kamal', 'HR', 30, 40000],
+    [4, 'Nipu', 'Project Manager', 45, 250000],
+    [5, 'Rajon', 'CEO', 60, 360000]
+];
+
+$names = ['orange', 'orange', 'banana', 'jackfruit'];
+
+echo sizeof($employees) . '<br>';
+echo count($employees, 1);
+
+echo '<pre>';
+print_r(array_count_values($names));
+echo '</pre>';
